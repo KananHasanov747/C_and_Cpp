6e210a88365c638d3c806daa4e2ece5056e6e736
Кянан Гасанзаде <Kanan2003@Kanans-MacBook-Air.local>
2022-04-21 00:12:38 +0400

    change Calculator

--- C++/Программирование с Бьярне Страуструпом/Chapter 6/Calculator.cpp
+++ C++/Программирование с Бьярне Страуструпом/Chapter 6/Calculator.cpp
@@ -1,228 +1,192 @@
-// The desk calculator
+//
+// Stroustrup - Programming Principles & Practice
+//
+// Chapter 6 - Calculator
+//
+// Try This #3 - Get final chapter 6 version of calculator to run
+//
+// ex 2 - Add the ability to use {} so that {(4+5)*6}/(3+4)}; is valid
+//
+// ex 3 - Add a factorial operator '!'. Make '!' bind tighter than '*' & '/'
+//
 
-// includes character-level input (sec6.1.3), but
-// no command line input (sec6.1.7),
-// no namespaces, and
-// no exceptions
+#include "std_lib_facilities.h"
 
-// pp 107-117, sec 6.1, A Desk calculator
+class Token {
+public:
+    char kind;
+    double value;
 
-
-// uses += rather than push_back() for string
-// to work around standard library bug
-
-// No guarantees offered. Constructive comments to bs@research.att.com
-
-
-/*
-    program:
-	END			   // END is end-of-input
-	expr_list END
-
-    expr_list:
-	expression PRINT	   // PRINT is semicolon
-	expression PRINT expr_list
-
-    expression:
-	expression + term
-	expression - term
-	term
-
-    term:
-	term / primary
-	term * primary
-	primary
-
-    primary:
-	NUMBER
-	NAME
-	NAME = expression
-	- primary
-	( expression )
-*/
-
-#include <string>
-#include <cctype>
-#include<iostream>
-#include<map>
-
-using namespace std;
-
-int no_of_errors;	// note: default initialized to 0
-
-double error(const char* s)
-{
-    no_of_errors++;
-    cerr << "error: " << s << '\n';
-    return 1;
-}
-
-enum Token_value {
-	NAME,		    NUMBER,		    END,
-	PLUS = '+',	    MINUS = '-',	MUL = '*',	    DIV = '/',
-	PRINT = ';',	ASSIGN = '=',	LP = '(',		RP = ')'
+    Token(char k) : kind{k}, value{0} { }
+    Token(char k, double v) : kind{k}, value{v} { }
 };
 
-Token_value curr_tok = PRINT;
-double number_value;
-string string_value;
+class Token_stream {
+public:
+    Token get();                // get a Token
+    void putback(Token t);      // put a token back
+private:
+    bool full { false };        // is there a Token in the buffer?
+    Token buffer {'0'};         // here is where putback() stores a Token
+};
 
-/* The simplest token reader
-
-Token_value get_token()
+void Token_stream::putback(Token t)
 {
-	char ch = 0;
-	cin>>ch;
+    buffer = t;                 // copy t to buffer
+    full = true;                // buffer is now full
+};
 
-	switch (ch) {
-	case 0:
-		return curr_tok=END;
-	case ';':
-	case '*':
-	case '/':
-	case '+':
-	case '-':
-	case '(':
-	case ')':
-	case '=':
-		return curr_tok=Token_value(ch);
-	case '0': case '1': case '2': case '3': case '4':
-	case '5': case '6': case '7': case '8': case '9':
-	case '.':
-		cin.putback(ch);
-		cin >> number_value;
-		return curr_tok=NUMBER;
-	default:					// NAME, NAME =, or error
-		if (isalpha(ch)) {
-			cin.putback(ch);
-			cin>>string_value;
-			return curr_tok=NAME;
-		}
-		error("bad token");
-		return curr_tok=PRINT;
-	}
-}
-*/
-
-Token_value get_token ()
+Token Token_stream::get()
+    // ex 2 - added '{' & '}'
+    // ex 3 - added '!'
 {
-	char ch;
+    if (full) {             // do we already have a Token?
+        full = false;       // remove Token from buffer
+        return buffer;
+    }
 
-	do {	// skip whitespace except '\en'
-		if (!cin.get(ch)) return curr_tok = END;
-	} while (ch!='\n' && isspace(ch));
+    char ch;
+    cin >> ch;              // note that >> skips whitespace
 
-	switch (ch) {
-	case ';':
-	case '\n':
-		return curr_tok = PRINT;
-	case '*':
-	case '/':
-	case '+':
-	case '-':
-	case '(':
-	case ')':
-	case '=':
-		return curr_tok = Token_value (ch);
-	case '0': case '1': case '2': case '3': case '4':
-	case '5': case '6': case '7': case '8': case '9':
-	case '.':
-		cin.putback (ch);
-		cin >> number_value;
-		return curr_tok = NUMBER;
-	default:			// NAME, NAME=, or error
-		if (isalpha (ch)) {
-			string_value = ch;
-			while (cin.get (ch) && isalnum (ch))
-				string_value += ch;	// string_value.push_back(ch);
-							// to work around library bug
-			cin.putback (ch);
-			return curr_tok = NAME;
-		}
-		error("bad token");
-		return curr_tok = PRINT;
-	}
+    switch (ch) {
+        case ';':           // for "print"
+        case 'q':           // for "quit"
+        case '(': case ')': case '{': case '}': case '!':
+        case '+': case '-': case '*': case '/':
+            return Token { ch };
+        case '.':
+        case '0': case '1': case '2': case '3': case '4':
+        case '5': case '6': case '7': case '8': case '9':
+            {
+                cin.putback(ch);    // put digit back into input stream
+                double val;
+                cin >> val;
+                return Token { '8', val };  // let '8' represent a number
+            }
+        default:
+            error("Bad token");
+    }
+};
+
+Token_stream ts;            // provides get() and putback()
+double expression();        // forward declaration for primary to call
+
+double primary()            // deal with numbers and parenthesis
+    // ex 2 - added '{' case
+{
+    Token t = ts.get();
+    switch (t.kind) {
+        case '(':                   // handle '(' expression ')'
+            {
+                double d = expression();
+                t = ts.get();
+                if (t.kind != ')') error("')' expected");
+                return d;
+            }
+        case '{':
+            {
+                double d = expression();
+                t = ts.get();
+                if (t.kind != '}') error("'}' expected");
+                return d;
+            }
+        case '8':                   // we use '8' to represent a number
+            return t.value;         // return the number's value
+        default:
+            error("primary expected");
+    }
 }
 
-map <string, double> table;
-
-double expr (bool);	// cannot do without
-
-
-double prim (bool get)		// handle primaries
+double secondary()          
+    // ex 3 - Add a factorial operator '!'
 {
-	if (get) get_token ();
+    double left = primary();
+    Token t = ts.get();
 
-	switch (curr_tok) {
-	case NUMBER:		// floating-point constant
-	{	double v = number_value;
-		get_token ();
-		return v;
-	}
-	case NAME:
-	{	double& v = table [string_value];
-		if (get_token () == ASSIGN) v = expr (true);
-		return v;
-	}
-	case MINUS:		// unary minus
-		return -prim (true);
-	case LP:
-	{	double e = expr (true);
-		if (curr_tok != RP) return error (") expected");
-		get_token ();		// eat ')'
-		return e;
-	}
-	default: return error ("primary expected");
-	}
+    while (true) {
+        if (t.kind == '!') {
+            if (left == 0)
+                return 1;
+
+            for (int i = left - 1; i > 0; --i)
+                left *= i;
+
+            t = ts.get();
+        }
+        else {
+            ts.putback(t);
+            return left;
+        }
+    }
 }
 
-double term (bool get)		// multiply and divide
+double term()               // deal with * and /
 {
-	double left = prim (get);
+    double left = secondary();
+    Token t = ts.get();             // get next token from Token_stream
 
-	for (;;)
-		switch (curr_tok) {
-		case MUL:
-			left *= prim (true);
-			break;
-		case DIV:
-			if (double d = prim (true)) {
-				left /= d;
-				break;
-			}
-			return error ("divide by 0");
-		default: return left;
-		}
+    while (true) {
+        switch (t.kind) {
+            case '*':
+                left *= secondary();
+                t = ts.get();
+                break;
+            case '/':
+                {
+                    double d = secondary();
+                    if (d == 0) error("divide by zero");
+                    left /= d;
+                    t = ts.get();
+                    break;
+                }
+            default:
+                ts.putback(t);      // put t back into the Token_stream
+                return left;
+        }
+    }
 }
 
-double expr (bool get)		// add and subtract
+double expression()         // deal with + and -
 {
-	double left = term (get);
+    double left = term();           // read and evaluate a term
+    Token t = ts.get();             // get next token from Token_stream
 
-	for (;;)				// ``forever''
-		switch (curr_tok) {
-		case PLUS:
-			left += term (true);
-			break;
-		case MINUS:
-			left -= term (true);
-			break;
-		default: return left;
-		}
+    while (true) {
+        switch (t.kind) {
+            case '+':
+                left += term();     // evaluate term and add
+                t = ts.get();
+                break;
+            case '-':
+                left -= term();     // evaluate term and subtract
+                t = ts.get();
+                break;
+            default:
+                ts.putback(t);      // put t back into the token stream
+                return left;
+        }
+    }
 }
 
 int main()
-{
+try {
+    double val = 0;
+    while (cin) {
+        Token t = ts.get();
 
-	table ["pi"] = 3.1415926535897932385;	// insert predefined names
-	table ["e"] = 2.7182818284590452354;
-
-	while (cin) {
-		get_token ();
-		if (curr_tok == END) break;
-		if (curr_tok == PRINT) continue;
-		cout << expr (false) << '\n';
-	}
-
-	return no_of_errors;	
+        if (t.kind == 'q') break;       // 'q' for "quit"
+        if (t.kind == ';')              // ';' for "print now"
+            cout << "=" << val << '\n';
+        else
+            ts.putback(t);
+        val = expression();
+    }
+}
+catch(exception& e) {
+    cerr << "Exception: " << e.what() << '\n';
+    return 1;
+}
+catch(...) {
+    cerr << "Unknown exception\n";
+    return 2;
 }
